e5f588152ef27594d38e7f8c986657d551dc5b7d
joaorodrigues <joao.rodrigues.jr21@hotmail.com>
2025-09-08 11:50:57 +0100

    Edited the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
-
 const Footer = () => {
-  return (
-      <div className="footer">
-          <div className="container">
-              <p>© 2024 TechNexus. Created by João Rodrigues. All rights reserved.</p>
-          </div>
-    </div>
-  )
-}
+	return (
+		<div className='footer'>
+			<div className='container'>
+				<p>© {new Date().getFullYear()} TechNexus. Created by João Rodrigues. All rights reserved.</p>
+			</div>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
